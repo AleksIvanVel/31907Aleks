af20e192865f0c46cca8732bd3a0ac5b82a76abc
AleksIVanVel <aleksvelazquez32@aragon.unam.mx>
2023-11-13 11:50:23 -0600

    submit de registro de usuario

---
+++ proyecto_final/submit_registro.php
@@ -0,0 +1,50 @@
+<?php
+    //conexion a la base datos
+    include "conexion.php";
+    mysqli_set_charset($conexion,'utf8');
+
+    //declaracion de varibales para formulario
+    $buscarUsuario = "SELECT * FROM usuarios where nombre_usuario = '$_POST[nombre_usuario]'";
+    $result = $conexion -> query($buscarUsuario);
+    $count = mysqli_num_rows($result);
+
+    if($count ==1 ){
+        echo'El nombre se usuario ya a sido ocupado';
+        header('Location: ./reistrar_usuario.php');
+        
+    }else{
+        mysqli_query($conexion, "INSERT INTO usuarios (
+        nombre,
+        direccion,
+        telefono,
+        correo,
+        nombre_usuario,
+        password)
+            VALUES(
+        '$_POST[usuario]',
+        '$_POST[direccion]',
+        '$_POST[telefono]',
+        '$_POST[correo]',
+        '$_POST[nombre_usuario]',
+        '$_POST[password]'        
+        )");
+
+    echo "<br/><br/><br/>" . "<h1>" . "Usuario Registrado Exitosamente!" . "</h1>";
+    echo "<br/><br/>"."<h2>" . "Bienvenido: " . $_POST['usuario'] . "</h2>" . "\n\n";
+    echo "<br/><br/>"."<p>" . "<a href='./registrar_usuario.php'>Regresar al registro</a>" . "</p>";
+
+    //termina el else
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro</title>
+    <link rel="stylesheet" href="recursos/css/estilo_submit_registro.css">
+</head>
+<body>
+    
+</body>
+</html>
